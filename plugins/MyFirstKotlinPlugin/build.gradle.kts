ef24092b5730d101a60dff78db193d12a14366c2
wibrowelias-art <wibrowelias@gmail.com>
2026-08-31 14:10:50 +0200

    Update build.gradle.kts for Kotlin plugin configuration

--- plugins/MyFirstKotlinPlugin/build.gradle.kts
+++ plugins/MyFirstKotlinPlugin/build.gradle.kts
@@ -1,26 +1,30 @@
-version = "1.0.0" // Plugin version. Increment this to trigger an update
-description = "My first Kotlin plugin!" // Plugin description that will be shown to user
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.aliucord)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.github.yournamehere"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
 
 aliucord {
-    // Changelog of your plugin
-    changelog.set(
-        """
-        # 1.0.0
-        * Initial plugin release!
-        """.trimIndent(),
-    )
-    // Image or Gif that will be shown at the top of your changelog page
-    // changelogMedia.set("https://cool.png")
+    author("YourName", 0L)
+    github("https://github.com/wibrowelias-art/Customprofile")
+}
 
-    // Add additional authors to this plugin
-    // author("Name", 0L, hyperlink = true)
-    // author("Name", 0L, hyperlink = true)
-
-    // Excludes this plugin from publishing and global plugin repositories.
-    // Set this to false if the plugin is unfinished
-    deploy.set(false)
-
-    // Builds and deploys this plugin but excludes it from global plugin repositories.
-    // Set this if the plugin has reached EOL but a last update should still occur.
-    // deployHidden.set(true)
+dependencies {
+    compileOnly(libs.discord)
+    compileOnly(libs.aliucord)
+    compileOnly(libs.kotlin.stdlib)
 }
