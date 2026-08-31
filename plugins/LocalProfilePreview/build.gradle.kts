046299cb65f4c6085ee25c590402c7370d771787
wibrowelias-art <wibrowelias@gmail.com>
2026-08-31 16:02:59 +0200

    Update build.gradle.kts for Android library configuration

--- plugins/LocalProfilePreview/build.gradle.kts
+++ plugins/LocalProfilePreview/build.gradle.kts
@@ -3,9 +3,32 @@ plugins {
     id("com.aliucord.gradle")
     kotlin("android")
 }
+
 android {
     namespace = "com.github.yournamehere"
     compileSdk = 34
-    defaultConfig { minSdk = 21 }
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
+dependencies {
+    compileOnly(libs.discord)
+    compileOnly(libs.aliucord)
+    compileOnly(libs.androidx.core)
+    compileOnly(libs.androidx.fragment)
+}
+
+aliucord {
+    author("wibrowelias-art", 0L)
+    github("https://github.com/wibrowelias-art/Customprofile")
+}
